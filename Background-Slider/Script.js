a6a9f5bed899056580e5d068d8867eca8271e0d4
Prasanna <prasannagnihotri24@gmail.com>
2021-10-08 00:07:30 +0530

    Changed the fourth image

--- Background-Slider/Script.js
+++ Background-Slider/Script.js
@@ -2,7 +2,7 @@ const bg = [
   "https://images.unsplash.com/photo-1520338801623-6b88fe32bbf2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
   "https://images.unsplash.com/photo-1486748719772-dac71e23eaa1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
   "https://images.unsplash.com/photo-1473889051109-2de98686f4c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1498&q=80",
-  "https://images.unsplash.com/photo-1463277055586-70022e9cf2b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80",
+  "https://images.unsplash.com/photo-1606089397043-89c1758008e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
   "https://images.unsplash.com/photo-1626271763156-520913751759?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80",
 ];
 const body = document.querySelector("body");
